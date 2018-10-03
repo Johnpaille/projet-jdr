1cd2ab95fbe7caa011866645f43e29a40374ba17
Johnpaille <koehlercyril@gmail.com>
2018-10-03 13:58:16 +0200

    Ajout du templating twig

--- src/AppBundle/Controller/GeniousController.php
+++ src/AppBundle/Controller/GeniousController.php
@@ -11,6 +11,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating;
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
 
 class GeniousController extends Controller {
     /**
